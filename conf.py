ce220c4b42d8a79314b17a6ec0842d0c534e8ad8
Alen Rubilar <lclc.alen@gmail.com>
2025-07-13 14:08:27 -0700

    Update Sphinx configuration in conf.py to include additional extensions for documentation generation. Modify pyproject.toml to add new dependencies for Sphinx extensions and update existing ones. Enhance uv.lock with new package entries for improved project functionality.

--- conf.py
+++ conf.py
@@ -6,22 +6,26 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'secure_transformer'
-copyright = '2025, Alen Rubilar'
-author = 'Alen Rubilar'
+project = "secure_transformer"
+copyright = "2025, Alen Rubilar"
+author = "Alen Rubilar"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+extensions = [
+    "sphinx.ext.autodoc",  # pull docstrings
+    "sphinx.ext.napoleon",  # Google / NumPy style
+    "sphinx.ext.autosummary",  # tables of APIs with links
+    "sphinx.ext.intersphinx",  # cross-project links
+]
+autodoc_typehints = "description"
+autosummary_generate = True
 
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
